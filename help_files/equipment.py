7a79212b917e37facbd8b380ed75e4c2738afeec
printhiworld <e.muhina@it-park.tech>
2023-05-19 22:19:46 +0300

    3

--- help_files/equipment.py
+++ help_files/equipment.py
@@ -55,7 +55,7 @@ class Equipment:
 
     @staticmethod
     def _get_equipment_data() -> EquipmentData:
-        equipment_file = open("./data/equipment.json")
+        equipment_file = open("./data/equipment.json", encoding='utf-8')
         data = json.load(equipment_file)
         equipment_schema = marshmallow_dataclass.class_schema(EquipmentData)
         try:
